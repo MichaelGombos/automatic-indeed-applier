66739486645bf5a52aadcb18c8c0c3edc0ce4607
Michael Gombos <gombos2002@gmail.com>
2024-02-03 21:10:41 -0600

    add placeholder dashboard

---
+++ dashboard/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Indeed Bot V1:</h1>
+        <p>Read output via console  <br/>Send commands via terminal</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
