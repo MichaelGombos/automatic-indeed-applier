2fa9de45ae3dedb9ae4d43cd70208ad9c8e816e0
Michael Gombos <gombos2002@gmail.com>
2024-02-04 00:07:46 -0600

    add basic route handlers to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let posts = [
   {
     id: 1,
@@ -44,6 +46,68 @@ app.get("/", (request, response) => {
 
 app.get("/api/posts", (request, response) => {
   response.json(posts);
+});
+
+app.get("/api/posts/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const post = posts.find((post) => {
+    console.log(post.id, typeof post.id, id, typeof id, post.id === id);
+    return post.id === id;
+  });
+
+  if (post) {
+    response.json(post);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/posts/:id", (request, response) => {
+  const id = Number(request.params.id);
+  posts = posts.filter((post) => post.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = posts.length > 0 ? Math.max(...posts.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/posts", (request, response) => {
+  console.log("DID THE REQUEST COME THROUGH");
+  console.log(request.body);
+
+  const post = request.body;
+  post.id = generateId();
+
+  posts = posts.concat(post);
+
+  response.json(post);
+});
+
+app.delete("/api/posts/:id", (request, response) => {
+  const id = Number(request.params.id);
+  posts = posts.filter((post) => post.id !== id);
+
+  response.status(204).end();
+});
+
+app.put("/api/posts/:id", (request, response) => {
+  console.log(request.body);
+  const id = Number(request.params.id);
+  postIndex = posts.findIndex((p) => p.id === id);
+
+  const post = request.body;
+  if (postIndex) {
+    post.id = id;
+    posts[postIndex] = post;
+    response.json(post);
+  } else {
+    //post using regular app.post request
+    console.log("failed PUT: post not found");
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
